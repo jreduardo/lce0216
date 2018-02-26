2b738d66e45a8ebeacd373e187418194210584ed
Eduardo Junior <edujrrib@gmail.com>
2018-02-26 17:30:30 -0300

    Corrige deploy

--- deploy.sh
+++ deploy.sh
@@ -6,6 +6,7 @@ git config --global user.name "Travis boot to deploy pdf files"
 git clone -b gh-pages https://${GIT_KEY}@github.com/jreduardo/lce0216 dir-output
 cp *.pdf aulas/*.pdf dir-output/
 
+cd dir-output/
 git add --all *
 git commit -m "Automatic Update (build number ${TRAVIS_BUILD_NUMBER})" || true
 git push origin gh-pages
