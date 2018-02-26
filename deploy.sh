abead9e87716deaa97ff1b3469e63a9c899fb4aa
Eduardo Junior <edujrrib@gmail.com>
2018-02-26 17:15:00 -0300

    Adiciona esquema para deploy de arquivos pdf

---
+++ deploy.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+git config --global user.email "edujrrib@gmail.com"
+git config --global user.name "Travis boot to deploy pdf files"
+
+git clone -b gh-pages https://${GIT_KEY}@github.com/jreduardo/lce0216 dir-output
+cp *.pdf aulas/*.pdf dir-output/
+
+git add --all *
+git commit -m "Automatic Update (build number ${TRAVIS_BUILD_NUMBER})" || true
+git push origin gh-pages
